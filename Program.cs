c9d56161e33b5005b1f080acd3addfc9cc2a392f
Matheus Luan <matheusluan04@gmail.com>
2026-05-29 21:58:58 -0300

    Finalizção de tarefa

---
+++ Program.cs
@@ -0,0 +1,128 @@
+﻿string opcao = "", nome = "";
+decimal saldo = 0, chequeEspecial = 0;
+bool isContaCriada = false;
+
+const string TITULO = "---- MongaBank - Seu dinheiro rende mais! ----\n";
+
+const string MENU = @"Selecione uma opção abaixo:
+
+0 - Abrir Conta Corrente
+1 - Saldo
+2 - Saque
+3 - Depósito
+4 - Encerrar conta e sair
+
+Opção: ";
+
+
+
+while (true)
+{
+    Console.Clear();
+    
+    Console.WriteLine(TITULO);   
+    Console.Write(MENU);
+
+    opcao = Console.ReadLine()!.Trim().Substring(0,1);
+
+    Console.WriteLine($"Opção informada: {opcao}");
+
+    if (opcao == "0")
+    {
+        Console.Clear();
+        Console.WriteLine(TITULO); 
+        Console.Write("Nome: ");
+        nome = Console.ReadLine()!;
+
+        Console.Write("Depósito Inicial: ");
+        saldo = Convert.ToDecimal(Console.ReadLine());
+
+        Console.Write("Cheque especial: ");
+        chequeEspecial = Convert.ToDecimal(Console.ReadLine());
+
+        Console.WriteLine($"\nBem vindo, {nome}.");
+        Console.WriteLine($"Seu saldo é de {saldo:C2}");
+        Console.WriteLine($"Seu limite de cheque especial é de {chequeEspecial:C2}\n");
+
+        isContaCriada = true;
+    }
+    else if (!isContaCriada)
+    {
+        Console.WriteLine("A abertura da conta é obrigatória!!");
+
+        Console.Write("Pressione uma tecla para continuar");
+        Console.ReadKey(true);
+        continue;
+    }
+    else 
+    {
+        Console.Clear();
+        Console.WriteLine(TITULO);
+        Console.WriteLine($"Cliente: {nome}");
+
+        if (opcao == "1"){ 
+            Console.WriteLine($"Seu saldo é de: {saldo:C2}");
+            Console.WriteLine($"Limite de cheque especial: {chequeEspecial:C2}");
+        }
+        else if (opcao == "2")
+        {
+            Console.Write("Informe um valor para saque: ");
+            decimal valorSaque = Convert.ToDecimal(Console.ReadLine());
+
+            bool valorEhValido = valorSaque > 0;
+            bool limiteSuficiente = saldo + chequeEspecial >= valorSaque;
+            bool estaUsandoLimite = valorSaque > saldo;
+
+            if(!valorEhValido)
+            {
+                Console.WriteLine("-> Não foi possível realizar sua operação. Tente novamente");
+            }
+            else if(!limiteSuficiente)
+            {
+                Console.WriteLine("-> Seu limite atual não permite esta operação!");
+            }
+            else
+            {
+                if(estaUsandoLimite)
+                {
+                    Console.WriteLine("-> Você está utilizando seu cheque especial");
+                }
+                saldo -= valorSaque;
+                Console.WriteLine("Saque efetuado com sucesso.");
+                Console.WriteLine($"-> Seu saldo é de {saldo:C2}");
+            }
+        }
+        else if (opcao == "3")
+        {
+            Console.Write("Informe um valor para depósito: ");
+            decimal valorDeposito = Convert.ToDecimal(Console.ReadLine());
+
+            bool valorEhValido = valorDeposito > 0;
+            if(!valorEhValido)
+            {
+                Console.WriteLine("-> Não foi possível realizar sua operação. Tente novamente");
+            }
+            else
+            {
+                saldo += valorDeposito;
+                Console.WriteLine("Depósito efetuado com sucesso.");
+                Console.WriteLine($"-> Seu saldo é de {saldo:C2}");
+            }
+        }
+        else if (opcao == "4")
+        {
+            Console.WriteLine("Obrigado por utilizar nossos serviços");
+            Console.WriteLine($"-> Valor a receber: {saldo:C2}");
+            break;
+        }
+        else
+        {
+            Console.WriteLine("Opção Inválida!!!");
+        }
+
+    }
+
+    Console.Write("Pressione uma tecla para continuar");
+    Console.ReadKey(true);
+
+}
